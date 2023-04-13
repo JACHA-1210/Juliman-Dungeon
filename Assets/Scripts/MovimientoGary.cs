51aa242b485a5fca24f137e6a7cc4d5c979095df
yuberino <xavieryubero@ceroca.cat>
2023-04-13 16:54:39 +0200

    Animacion del personaje mejorada

--- Assets/Scripts/MovimientoGary.cs
+++ Assets/Scripts/MovimientoGary.cs
@@ -7,6 +7,8 @@ public class MovimientoGary : MonoBehaviour
 
     public float _velGary;
 
+    private SpriteRenderer spritePersonaje;
+
     public GameObject Gary;
 
     private Animator anim;
@@ -17,19 +19,20 @@ public class MovimientoGary : MonoBehaviour
     {
         anim = GetComponentInChildren<Animator>();
         _velGary = 4f;
+        spritePersonaje = GameObject.Find("Animador").GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Moverse
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        {
-            anim.SetTrigger("Moviendose");
-        }
 
         float direccioX = Input.GetAxisRaw("Horizontal");
         float direccioY = Input.GetAxisRaw("Vertical");
+
+        float totalDireccio = Mathf.Abs(direccioX) + Mathf.Abs(direccioY);
+
+        anim.SetFloat("Moverse", Mathf.Abs(totalDireccio));
 
         Vector2 direccioIndiacada = new Vector2(direccioX, direccioY).normalized;
 
@@ -39,12 +42,34 @@ public class MovimientoGary : MonoBehaviour
 
         transform.position = posGary;
 
-        //Ataque espada 
-        if (Input.GetMouseButtonDown(0))
+        if (direccioX < 0)
         {
-            anim.SetTrigger("Atacar");
+
+            spritePersonaje.flipX = true;
+
         }
 
+        else if (direccioX > 0)
+        {
+
+            spritePersonaje.flipX = false;
+
+        }
+
+        //Ataque espada 
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetFloat("Moverse", Mathf.Abs(0));
+            anim.SetFloat("Atacar", Mathf.Abs(1));
+        }
+
+        // Verificar si la animación de "Atacar" ha terminado
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        if (stateInfo.IsName("Atacar") && stateInfo.normalizedTime >= 1.0f)
+        {
+            // Restablecer el valor del float "Atacar" a 0
+            anim.SetFloat("Atacar", 0);
+        }
 
     }
 }
