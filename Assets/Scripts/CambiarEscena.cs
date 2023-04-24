9779c0cddf655a742e4f7e743575cdcd5c0a5b90
yuberino <xavieryubero@ceroca.cat>
2023-04-24 20:29:59 +0200

    Animations

--- Assets/Scripts/CambiarEscena.cs
+++ Assets/Scripts/CambiarEscena.cs
@@ -5,10 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class CambiarEscena : MonoBehaviour
 {
-    //
+    private string nombreEscena1;
+    private Animator animator;
+
+    void Start()
+    {     
+        animator = GetComponent<Animator>();           
+    }
+
     public void cargarEscena(string nombreEscena)
     {
-        SceneManager.LoadScene(nombreEscena);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+        nombreEscena1 = nombreEscena;
+        animator.SetTrigger("StartTransition");
+        
+        Invoke("test", 1f);
+    }
+
+    public void test()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nombreEscena1);
     }
 }
