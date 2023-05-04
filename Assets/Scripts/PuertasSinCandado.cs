4698118de343191eb1bd62c954d557ca15915c0b
yuberino <xavieryubero@ceroca.cat>
2023-05-04 19:29:57 +0200

    PuertasFuncion

---
+++ Assets/Scripts/PuertasSinCandado.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuertasSinCandado : MonoBehaviour
+{
+
+    public GameObject _puertasClosed;
+    public GameObject _puertasOpen;
+    private bool estadoPuerta;
+    private bool tocandoPuerta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        estadoPuerta = false;
+        tocandoPuerta = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (tocandoPuerta)
+            {
+                InteractuarConPuerta();
+            }          
+        }
+
+        Debug.Log(tocandoPuerta);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            tocandoPuerta = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            tocandoPuerta = false;
+        }
+    }
+
+    public void InteractuarConPuerta()
+    {
+        if (!estadoPuerta)
+        {
+            _puertasClosed.SetActive(false);
+            _puertasOpen.SetActive(true);
+
+        }
+        else
+        {
+            _puertasClosed.SetActive(true);
+            _puertasOpen.SetActive(false);
+
+        }
+
+        estadoPuerta = !estadoPuerta;
+    }
+}
