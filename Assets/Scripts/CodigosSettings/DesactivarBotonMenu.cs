e79ae5239460395adf3625d49fdabf89b2ab29a5
Jose Andres Chero Ascurra <jose_andreschero@ceroca.cat>
2023-05-11 15:44:03 +0200

    Código traspasado

---
+++ Assets/Scripts/CodigosSettings/DesactivarBotonMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DesactivarBotonMenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene escenaActual = SceneManager.GetActiveScene();
+
+        // Comprobar si es la escena "SettingsGame"
+        if (escenaActual.name == "SettingsGame")
+        {
+            // Ocultar el bot�n si est� en la escena "SettingsGame"
+            gameObject.SetActive(false);
+        }
+        else if (escenaActual.name == "JugarNivel1")
+        {
+            // Mostrar el bot�n si est� en la escena "JugarNivel1"
+            gameObject.SetActive(true);
+        }
+    }
+}
