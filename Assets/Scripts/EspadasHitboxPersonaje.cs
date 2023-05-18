ee417e3e9c2df3da694aab8b8e328fbc90044de3
yubeniro <xavieryubero@ceroca.cat>
2023-05-18 12:55:14 +0200

    Zombie Stuff Pt.2

--- Assets/Scripts/EspadasHitboxPersonaje.cs
+++ Assets/Scripts/EspadasHitboxPersonaje.cs
@@ -8,11 +8,7 @@ public class EspadasHitboxPersonaje : MonoBehaviour
     public GameObject espadaHitboxIzquierda;
     public GameObject espadaHitboxDerecha;
 
-    private bool colisionandoConZombie = false;
-
-    private bool controladorEspada = true;
-
-    private bool usandoEspada = false;
+    public bool colisionandoConZombie = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,44 +19,15 @@ public class EspadasHitboxPersonaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameObject.FindGameObjectWithTag("Enemic") != null)
-        {
-            if (Input.GetMouseButton(0) && !GameObject.Find("Gary").GetComponent<MovimientoGary>().siendoEmpujado)
-            {
-                usandoEspada = true;
-                Invoke("UsandoEspada", 0.8f);
-            }
 
-            if (colisionandoConZombie && usandoEspada && controladorEspada)
-            {
-                Invoke("EspadaTocaZombie", 0.1f);
-            }
-        }
-        
-    }
-
-    public void EspadaTocaZombie ()
-    {
-        GameObject.FindGameObjectWithTag("Enemic").GetComponent<Zombie>().DestroyZombie();
-        controladorEspada = false;
-        Invoke("CambiarControladorEspada", 0.8f);
-    }
-
-    public void CambiarControladorEspada ()
-    {
-        controladorEspada = true;
-    }
-
-    public void UsandoEspada()
-    {
-        usandoEspada = false;
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "ColZombie")
         {
-            colisionandoConZombie = true;
+            GameObject zombie = collision.gameObject;
+            Destroy(zombie);
         }
     }
 
